570ee7d4e55c0eb8db6d771e6477f9b7914d0171
SpiritQuaddicted <spirit@quaddicted.com>
2013-07-27 20:59:29 +0200

    translated my comments to english

--- quakeinjectorxml.php
+++ quakeinjectorxml.php
@@ -4,7 +4,8 @@ ob_start();
 //$time_start = microtime(true);
 
 // TODO
-// date ist hier dd.mm.yyyy, der injector will aber dd.mm.yy. injector vor nächstem release patchen, solange wird hier dd.mm.yy draus gemacht.  
+// date is currently formatted dd.mm.yyyy, the QI expects dd.mm.yy
+// patch QI before its next release, then remove the tinkering in here  
 
 $db = new SQLite3('/srv/http/quaddicted.sqlite');
 
@@ -12,15 +13,15 @@ $results = $db->query('SELECT * FROM maps
 LEFT OUTER JOIN (SELECT zipname, GROUP_CONCAT(bsp) AS startmaps FROM startmaps GROUP BY zipname) 
 AS group_subselectbsp ON group_subselectbsp.zipname = maps.zipname 
 LEFT OUTER JOIN (SELECT zipname, GROUP_CONCAT(dependency) AS dependencies FROM dependencies GROUP BY zipname) 
-AS group_subselectdep ON group_subselectdep.zipname = maps.zipname WHERE maps.type!=4 ORDER BY maps.zipname'); // currently excluding the speedmaps with that WHERE, TODO remove it once at least the techinfo was added for them
+AS group_subselectdep ON group_subselectdep.zipname = maps.zipname WHERE maps.type!=4 ORDER BY maps.zipname'); // currently excluding the speedmaps with that WHERE, TODO add them once at least the data was added for them
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<files>\n";
 while ($row = $results->fetchArray()) {
 	//print_r($row);
 	//echo "<hr />";
-	echo "<file id=\"".$row[2]."\" type=\"".$row['type']."\" "; // $row[2] ist ein hack, ich weiss nicht wo 'zipname' hin ist. :(
+	echo "<file id=\"".$row[2]."\" type=\"".$row['type']."\" "; // $row[2] is a hack, no idea why i cannot use 'zipname' :(
 	
-	// benutzt der injector doch garnicht!
+	// not currently used by the QI
 	/*
 	if ($row['hasbsp']==="1") { echo "hasbsp=\"1\" ";}
 	else { echo "hasbsp=\"0\" ";}
@@ -33,16 +34,16 @@ while ($row = $results->fetchArray()) {
 	*/
 	echo "rating=\"".$row['rating']."\">\n";
 	
-		echo "\t<author>".$row['author']."</author>\n"; // falls fehler beim xml parsen kommen hat wahrscheinlich jemand umlaute als html entities benutzt, das soll nicht sein
+		echo "\t<author>".$row['author']."</author>\n"; // if there are errors with the xml parsing, probably some editor forgot to encode & to &amp;
 		echo "\t<title>".$row['title']."</title>\n";
 		echo "\t<md5sum>".$row['md5sum']."</md5sum>\n";
 		echo "\t<size>".$row['size']."</size>\n";
-		echo "\t<date>".substr($row['date'],0,6).substr($row['date'],8,9)."</date>\n"; // FIXME, siehe TODO oben
+		echo "\t<date>".substr($row['date'],0,6).substr($row['date'],8,9)."</date>\n"; // FIXME, see TODO
 		echo "\t<description><![CDATA[".$row['description']."]]></description>\n";
 		echo "\t<techinfo>\n";
-			// eigentlich mit isset() checken oder? sind die etwa nicht NULL in der db?!
-			// leere techinfo sollte auch nicht rein ;) oder sind das eh nur die speedmaps bisher? die bekommen ja zumindest startmaps
-			if ($row['zipbasedir'] != "") { echo "\t\t<zipbasedir>".$row['zipbasedir']."</zipbasedir>\n"; } //TODO wieso gibt das auch leere tags?
+			// TODO if techinfo would be empty, it should be excluded
+			// TODO why don't i use isset() below?
+			if ($row['zipbasedir'] != "") { echo "\t\t<zipbasedir>".$row['zipbasedir']."</zipbasedir>\n"; } //TODO why can this lead to empty entries?
 			if ($row['commandline'] != "") { echo "\t\t<commandline>".$row['commandline']."</commandline>\n"; }
 			if ($row['startmaps'] != "") {
 				$startmaps = explode(",", $row['startmaps']);
