06dffb69d904c48f010f15e1925c13c1b1b74e59
Hannes <>
2020-05-23 11:45:17 +0200

    some fixes for the feeds, can't remember what and why

--- atom-for-frontpage.php
+++ atom-for-frontpage.php
@@ -7,18 +7,19 @@ echo "<feed xmlns=\"http://www.w3.org/2005/Atom\">\n";
 echo "        <title>The latest Quake singleplayer releases at Quaddicted.com</title>\n";
 echo "        <link href=\"https://www.quaddicted.com/reviews/atom.php\" rel=\"self\" />\n";
 echo "        <link href=\"https://www.quaddicted.com/reviews/\" />\n";
-echo "        <id>tag:quaddicted.com,2011-07-16:latest-spmaps-feed:/</id>\n";
-echo "        <updated>2003-12-13T18:30:02Z</updated>\n";
+echo "        <id>tag:quaddicted.com,".date(\DateTime::ATOM).":latest-spmaps-feed:/</id>\n";
+echo "        <updated>".date(\DateTime::ATOM)."</updated>\n";
 
 $dbq = new SQLite3('/srv/http/quaddicted.sqlite');
-$results = $dbq->query('SELECT id,author,zipname,title,description FROM maps ORDER BY id DESC LIMIT 10');
+$results = $dbq->query('SELECT id,timestamp,author,zipname,title,description FROM maps ORDER BY id DESC LIMIT 10');
 while ($row = $results->fetchArray()) {
 
 	echo "	<entry>\n";
-		echo "		<title type=\"html\">".htmlentities($row['title'])." by ".htmlentities($row['author'])."</title>\n";
+		echo "		<title type=\"html\">".htmlentities($row['title'], ENT_XML1)." by ".htmlentities($row['author'], ENT_XML1)."</title>\n";
 		echo "		<link href=\"https://www.quaddicted.com/reviews/".$row['zipname'].".html\" />\n";
-		echo "		<id>tag:quaddicted.com,2011-01-01:spmaps-mapid-".$row['id']."</id>\n";
-		echo "		<summary type=\"html\">".htmlentities($row['description']."<img src=\"https://www.quaddicted.com/reviews/screenshots/".$row['zipname'].".jpg\" />")."</summary>\n";
+		echo "		<id>tag:quaddicted.com,".date('Y-m-d', $row['timestamp']).":spmaps-mapid-".$row['id']."</id>\n";
+		echo "		<summary type=\"html\">".htmlentities($row['description']."<img src=\"https://www.quaddicted.com/reviews/screenshots/".$row['zipname'].".jpg\" />", ENT_XML1)."</summary>\n";
+		echo "		<updated>".date(\DateTime::ATOM, $row['timestamp'])."</updated>\n";  // changed from issued, 20180326
 	echo "	</entry>\n";
 }
 
--- recentactivity.php
+++ recentactivity.php
@@ -11,19 +11,18 @@ echo "<feed xmlns=\"http://www.w3.org/2005/Atom\">\n";
 echo "        <title>Recent user activity at Quaddicted.com</title>\n";
 echo "        <link href=\"https://www.quaddicted.com/reviews/recentactivity.php\" rel=\"self\" />\n";
 echo "        <link href=\"https://www.quaddicted.com/reviews/\" />\n";
-echo "        <id>tag:quaddicted.com,2013-10-04:recent-activity-feed:/</id>\n";
-echo "        <updated>2003-12-13T18:30:02Z</updated>\n";
+echo "        <id>tag:quaddicted.com,".date(\DateTime::ATOM).":recent-activity-feed:/</id>\n";
+echo "        <updated>".date(\DateTime::ATOM)."</updated>\n";
 
 $dbq = new SQLite3('/srv/http/quaddicted.sqlite');
-$results = $dbq->query('SELECT * FROM recentactivity ORDER BY timestamp DESC LIMIT 10');
+$results = $dbq->query('SELECT id,timestamp,username,string FROM recentactivity ORDER BY timestamp DESC LIMIT 10');
 while ($row = $results->fetchArray()) {
-
-	echo "	<entry>\n";
-		$time = round((($unixtime-htmlentities($row['timestamp']))/60),0);
-		//echo "		<title type=\"html\">&lt;b&gt;".htmlentities($row['username'])."&lt;/b&gt; ".htmlentities($row['string'])." &lt;small&gt;".$time." minutes ago.&lt;/small&gt;</title>\n"; // is used by dokuwiki
-		echo "		<title type=\"html\">A user ".htmlentities($row['string'])." &lt;small&gt;".$time." minutes ago.&lt;/small&gt;</title>\n"; // is used by dokuwiki
-		//echo "		<summary type=\"html\">&lt;b&gt;".htmlentities($row['username'])."&lt;/b&gt; ".htmlentities($row['string'])." ".$time." minutes ago.</summary>\n";
-	echo "	</entry>\n";
+  echo "  <entry>\n";
+  $time = round((($unixtime-htmlentities($row['timestamp']))/60),0);
+  echo "<title type=\"html\">A user ".htmlentities($row['string'], ENT_XML1)." &lt;small&gt;".$time." minutes ago.&lt;/small&gt;</title>\n"; // is used by dokuwiki // ENT_XML1 so things like &ldquo; is output as numeric entity rather, that is allowed, the name isnt
+  echo "<id>tag:quaddicted.com,".date('Y-m-d', $row['timestamp']).":recent-activity-feed-".$row['id']."</id>\n";
+  echo "<updated>".date(\DateTime::ATOM, $row['timestamp'])."</updated>\n";  // changed from issued, 20180326
+  echo "  </entry>\n";
 }
 
 echo "</feed>";
