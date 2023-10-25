8af44300ea1a547976dbde4706d6100cace42df2
SpiritQuaddicted <>
2023-10-25 21:03:48 +0200

    Old changes: Easter egg for a better world

--- _header.php
+++ _header.php
@@ -2,8 +2,15 @@
 include '../static/_cookieconsent.html';
 ?>
                 <div id="header">
-                        <a href="/"><img src="/static/top.png" alt="Quaddicted.com Logo" id="logo" /></a>
                         <div id="quakeinjector"><img src="/static/injector64.png" alt="Small Quake Injector Logo" />Easily install and launch Quake maps with the cross-platform <a href="/tools/quake_injector">Quake Injector</a></div>
+                        <a href="/">
+<?php
+if (rand(0, 500) <= 23) {
+	echo '<img src="/static/top_rainbow.png" alt="Surprise! An alternative, glorious Quaddicted.com Logo, for the openness and diversity in the Quake scene" id="logo" /></a>';
+} else {
+        echo '<img src="/static/top.png" alt="Quaddicted.com Logo" id="logo" width="748" height="115" /></a>';
+}
+?>
                         <br />
                         <br />
                         <span id="navlinks">
